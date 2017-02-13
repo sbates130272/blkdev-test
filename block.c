ac95bf96d0749cd0d12769a995efe4e91eb611b1
Stephen Bates <sbates@raithlin.com>
2017-02-12 20:58:38 -0700

    Minor fixes and started writing to the block device.

--- block.c
+++ block.c
@@ -7,12 +7,18 @@
 #include <unistd.h>
 #include <fcntl.h>
 #include <errno.h>
+#include <string.h>
+
+#define PAGE_SIZE 4096 /* Where is this defined */
+#define NUM_IO 10
 
 int main(int argc, char *argv[])
 {
-	int fd, ret;
+	int io, fd, ret;
 	off_t size;
+	ssize_t wrote;
 	struct stat buf;
+	char wdata[PAGE_SIZE];
 
 	/* Input argument check */
 
@@ -52,6 +58,21 @@ int main(int argc, char *argv[])
 	}
 	fprintf(stdout,"The size of %s appears to be %zd B.\n",
 		argv[1], size);
+
+	/* Perform some PAGE_SIZE byte random IO on the device. */
+	for ( io=0; io<NUM_IO; io++ ){
+		memset(wdata, io, 4096);
+		lseek(fd, io*PAGE_SIZE, SEEK_SET);
+		wrote = write(fd, wdata, PAGE_SIZE);
+		if ( wrote == -1 ){
+			perror("write");
+			return -1;
+		}
+		sync();
+		fprintf(stdout, "Performed write %2d/%2d (%d/%d)\r",
+			io+1, NUM_IO, io, io*PAGE_SIZE);
+	}
+	fprintf(stdout,"\n");
 		
 
 	close(fd);
